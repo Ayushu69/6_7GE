5b6dda605452bef5074f80d3f2af893c5b5c870b
Ayushmaan <ayushmaandhiman59@gmail.com>
2026-09-01 11:54:04 +0530

    Feat: Add clangd index files to cache for improved code navigation and analysis.

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,77 @@
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
 #include "Constants.h"
 #include "core/Game.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 // GLOBAL RENDERER
-void render(SDL_Renderer* renderer, Game& game) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+void render(SDL_Renderer *renderer, Game &game) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
 
-    game.render(renderer);
+  game.render(renderer);
 
-    SDL_RenderPresent(renderer);
+  SDL_RenderPresent(renderer);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-    // 1) SDL init check
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("SDL_Init failed: %s", SDL_GetError());
-        return 1;
-    }
+  // 1) SDL init check
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    SDL_Log("SDL_Init failed: %s", SDL_GetError());
+    return 1;
+  }
 
-    if(TTF_Init() != 0) {
-        SDL_Log("TTF_Init failed: %s", TTF_GetError());
-        return 1;
-    }
+  if (TTF_Init() != 0) {
+    SDL_Log("TTF_Init failed: %s", TTF_GetError());
+    return 1;
+  }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "six_sevenGE",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        kWindowWidth, kWindowHeight,
-        SDL_WINDOW_SHOWN
-    );
+  SDL_Window *window = SDL_CreateWindow("six_sevenGE", SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED, kWindowWidth,
+                                        kWindowHeight, SDL_WINDOW_SHOWN);
 
-    // 2) Window creation check
-    if (window == nullptr) {
-        SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(
-        window, 
-        -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-    );
-
-    // 3) Renderer creation check
-    if (renderer == nullptr) {
-        SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-    {
-        Game game(renderer);
-        bool running = true;
-
-        Uint64 now = SDL_GetPerformanceCounter();
-        Uint64 last = now;
-        float dt = 0.0f;
-
-        while (running) {
-            last = now;
-            now = SDL_GetPerformanceCounter();
-            dt = (float)(now - last) / SDL_GetPerformanceFrequency();
-
-            // GAME SYSTEM (EVENTS, UPDATION, RENDERING)
-            running = game.handleEvents();
-            game.tick(static_cast<float>(dt));
-            render(renderer, game);
-        }
-    }
-    
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_Quit();
+  // 2) Window creation check
+  if (window == nullptr) {
+    SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
     SDL_Quit();
-    
-    return 0;
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  // 3) Renderer creation check
+  if (renderer == nullptr) {
+    SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+  {
+    Game game(renderer);
+    bool running = true;
+
+    Uint64 now = SDL_GetPerformanceCounter();
+    Uint64 last = now;
+    float dt = 0.0f;
+
+    while (running) {
+      last = now;
+      now = SDL_GetPerformanceCounter();
+      dt = (float)(now - last) / SDL_GetPerformanceFrequency();
+
+      // GAME SYSTEM (EVENTS, UPDATION, RENDERING)
+      running = game.handleEvents();
+      game.tick(static_cast<float>(dt));
+      render(renderer, game);
+    }
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  TTF_Quit();
+  SDL_Quit();
+
+  return 0;
 }
